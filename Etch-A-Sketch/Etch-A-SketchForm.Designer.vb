544e19dd48c1196f65d1f16865d49c7bd3feb593
Zac <zac.minidude140@Gmail.com>
2023-12-02 09:57:48 -0700

    Added Context Strip

--- Etch-A-Sketch/Etch-A-SketchForm.Designer.vb
+++ Etch-A-Sketch/Etch-A-SketchForm.Designer.vb
@@ -22,17 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.ClearButton = New System.Windows.Forms.Button()
         Me.SelectColorButton = New System.Windows.Forms.Button()
         Me.DrawWaveformsButton = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.FileContextStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitContextStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditContextStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectColorContextStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawWaveformsContextStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearContextStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpContextStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutContextStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip.SuspendLayout()
         Me.SuspendLayout()
         '
         'DrawingPictureBox
         '
         Me.DrawingPictureBox.BackColor = System.Drawing.Color.BlanchedAlmond
+        Me.DrawingPictureBox.ContextMenuStrip = Me.ContextMenuStrip
         Me.DrawingPictureBox.Location = New System.Drawing.Point(11, 42)
         Me.DrawingPictureBox.Name = "DrawingPictureBox"
         Me.DrawingPictureBox.Size = New System.Drawing.Size(784, 330)
@@ -75,6 +87,63 @@ Partial Class Form1
         Me.DrawWaveformsButton.Text = "&Draw Waveforms"
         Me.DrawWaveformsButton.UseVisualStyleBackColor = True
         '
+        'ContextMenuStrip
+        '
+        Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileContextStripMenuItem, Me.EditContextStripMenuItem1, Me.HelpContextStripMenuItem})
+        Me.ContextMenuStrip.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(100, 70)
+        '
+        'FileContextStripMenuItem
+        '
+        Me.FileContextStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitContextStripMenuItem})
+        Me.FileContextStripMenuItem.Name = "FileContextStripMenuItem"
+        Me.FileContextStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FileContextStripMenuItem.Text = "File"
+        '
+        'ExitContextStripMenuItem
+        '
+        Me.ExitContextStripMenuItem.Name = "ExitContextStripMenuItem"
+        Me.ExitContextStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitContextStripMenuItem.Text = "Exit"
+        '
+        'EditContextStripMenuItem1
+        '
+        Me.EditContextStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorContextStripMenuItem, Me.DrawWaveformsContextStripMenuItem, Me.ClearContextStripMenuItem})
+        Me.EditContextStripMenuItem1.Name = "EditContextStripMenuItem1"
+        Me.EditContextStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.EditContextStripMenuItem1.Text = "Edit"
+        '
+        'SelectColorContextStripMenuItem
+        '
+        Me.SelectColorContextStripMenuItem.Name = "SelectColorContextStripMenuItem"
+        Me.SelectColorContextStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SelectColorContextStripMenuItem.Text = "Select Color"
+        '
+        'DrawWaveformsContextStripMenuItem
+        '
+        Me.DrawWaveformsContextStripMenuItem.Name = "DrawWaveformsContextStripMenuItem"
+        Me.DrawWaveformsContextStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.DrawWaveformsContextStripMenuItem.Text = "Draw Waveforms"
+        '
+        'ClearContextStripMenuItem
+        '
+        Me.ClearContextStripMenuItem.Name = "ClearContextStripMenuItem"
+        Me.ClearContextStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClearContextStripMenuItem.Text = "Clear"
+        '
+        'HelpContextStripMenuItem
+        '
+        Me.HelpContextStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutContextStripMenuItem1})
+        Me.HelpContextStripMenuItem.Name = "HelpContextStripMenuItem"
+        Me.HelpContextStripMenuItem.Size = New System.Drawing.Size(99, 22)
+        Me.HelpContextStripMenuItem.Text = "Help"
+        '
+        'AboutContextStripMenuItem1
+        '
+        Me.AboutContextStripMenuItem1.Name = "AboutContextStripMenuItem1"
+        Me.AboutContextStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.AboutContextStripMenuItem1.Text = "About"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -88,6 +157,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "Etch-A-Sketch"
         CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -97,4 +167,13 @@ Partial Class Form1
     Friend WithEvents ClearButton As Button
     Friend WithEvents SelectColorButton As Button
     Friend WithEvents DrawWaveformsButton As Button
+    Friend WithEvents ContextMenuStrip As ContextMenuStrip
+    Friend WithEvents FileContextStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitContextStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditContextStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SelectColorContextStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawWaveformsContextStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearContextStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpContextStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutContextStripMenuItem1 As ToolStripMenuItem
 End Class
